f0236ee0d0b585b23dfaa9e5d442123dfa58a529
Zehra Saçaklıdır <zehra@appcircle.io>
2025-06-14 17:48:54 +0300

    Added double quotes

--- git_clone.sh
+++ git_clone.sh
@@ -121,7 +121,7 @@ fi
             exit 1
         fi
 
-        runCommand git config --local --add http."${GIT_URL}".extraHeader "Authorization: Bearer ${GIT_PROVIDER_TOKEN}"
+        runCommand git config --local --add "http.${GIT_URL}.extraHeader" "Authorization: Bearer ${GIT_PROVIDER_TOKEN}"
     fi
 
     if [ ! -z "${GIT_EXTRA_PARAMS}" ] ; then
